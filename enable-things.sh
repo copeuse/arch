6df61b006196865f540663485ef5c4a6bf01e6d3
Alice <plafe.alice@gmail.com>
2025-07-24 03:21:21 +0200

    Add files via upload

---
+++ enable-things.sh
@@ -0,0 +1,7 @@
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+systemctl enable NetworkManager.service
+systemctl enable sddm.service
+systemctl --user enable pipewire pipewire-pulse wireplumber
+echo pense a configurer sudo en editant vsido et ajoute un utilisateur prune
+
