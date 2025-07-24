b8bab4331c3ae2623b6e6771f1affc2e8c5a6aba
Alice <plafe.alice@gmail.com>
2025-07-24 03:37:35 +0200

    Update enable-things.sh

--- enable-things.sh
+++ enable-things.sh
@@ -3,5 +3,6 @@ grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager.service
 systemctl enable sddm.service
 systemctl --user enable pipewire pipewire-pulse wireplumber
+chsh -s /usr/bin/fish
 echo pense a configurer sudo en editant vsido et ajoute un utilisateur prune
 
