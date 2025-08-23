e4acdb12a8d7071123ae132868bd5ceb280a77ee
Alice <plafe.alice@gmail.com>
2025-08-23 19:46:18 +0200

    Delete enable-things.sh

--- enable-things.sh
+++
@@ -1,8 +0,0 @@
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
-systemctl enable NetworkManager.service
-systemctl enable sddm.service
-systemctl --user enable pipewire pipewire-pulse wireplumber
-chsh -s /usr/bin/fish
-echo pense a configurer sudo en editant vsido et ajoute un utilisateur prune
-
